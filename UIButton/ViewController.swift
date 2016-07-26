0c78fc6a421ee2de122a5b6750c0414a476beacd
马锦航 <13839567147@163.com>
2016-07-26 19:37:54 +0800

    first commit

--- UIButton/ViewController.swift
+++ UIButton/ViewController.swift
@@ -10,18 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    let exampleButton: UIButton = {
+        var button = UIButton()
+        button.frame = CGRectMake(100, 100, 200, 40)
+        button.backgroundColor = UIColor.brownColor()
+        button.setTitle("There is Button", forState: UIControlState.Normal)
+//        button.addTarget(self, action: Selector(exampleButtonAction(button)), forControlEvents: UIControlEvents.TouchUpInside)
+        return button
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         
+        //  button作为参数传递的时候，参数列表 "_:"表示
+        exampleButton.addTarget(self, action: #selector(exampleButtonAction(_:)), forControlEvents: UIControlEvents.TouchUpInside)
+        
+        
+        //  貌似是2.2之前
+//        exampleButton.addTarget(self, action: Selector("exampleButtonAction:"), forControlEvents: UIControlEvents.TouchUpInside)
+        
+        view.addSubview(exampleButton)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    //Mark: - target
+    
+    //Mark: exampleButtonAction
+    func exampleButtonAction(sender: UIButton) -> Void {
+        
+        print("click")
+        
     }
-
+    
 
 }
 
